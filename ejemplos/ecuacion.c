9cbb51d81806823bfe231892e2ca7ce5111488a2
Jair Escamilla <hernandez_yo14@outlook.es>
2018-09-11 17:53:06 -0500

    Ejercicio numero 3

---
+++ ejemplos/ecuacion.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h> // Incluyo la libreria
+// Prototipoa de funciones
+void getCof(int*, int*, int*);
+void discriminante(int, int, int, int*);
+void calcula(int, int, int);
+// Funcion principal
+int main(void){
+  int a, b, c, d;
+  char r;
+  do {
+    getCof(&a, &b, &c);
+    discriminante(a, b, c, &d);
+    if (d >= 0) {
+      calcula(a, b, d);
+    }else{
+      printf("La ecuacion no tiene solucion\n");
+    }
+    printf("Desea hacer otra operacion? ");
+    scanf("%c", &r);
+    scanf("%c", &r);
+  } while(r == 's' || r == 'S');
+}
+void getCof(int* a, int* b, int*c){
+  printf("Ingresar coeficientes: ");
+  scanf("%d %d %d", a, b, c);
+}
+void discriminante(int a, int b, int c, int* d){
+  *d = (b*b) - (4*a*c);
+}
+void calcula(int a, int b, int d){
+  float x1 = 0.0, x2 = 0.0;
+  x1 = (float)(-(b) + sqrt(d))/2*a;
+  x2 = (float)(-(b) - sqrt(d))/2*a;
+  printf("Los reultados son: \n");
+  printf("x1 = %.2f\nx2 = %.2f\n", x1, x2);
+}
--- ejemplos/mayor.c
+++ ejemplos/mayor.c
@@ -4,23 +4,23 @@ void getNumber(int*);
 void comp(int*, int*);
 // Funcion principal
 int main(void){
-  int number = 0;
-  int mayor = 0;
+  int Number = 0;
+  int Mayor = 0;
   do {
-    getNumber(&number);
-    comp(&number, &mayor);
-  } while(number >= 0);
-  printf("El numero mayor es %d\n", mayor);
+    getNumber(&Number);
+    comp(&Number, &mMayor);
+  } while(Number >= 0);
+  printf("El numero mayor es %d\n", Mayor);
 }
 
 // Desarrollo de las funciones
-void getNumber(int* numero){
+void getNumber(int* Numero){
   printf("Ingresar siguiente numero: ");
-  scanf("%d", numero);
+  scanf("%d", Numero);
 }
 
-void comp(int* number, int* mayor){
-  if (*number > *mayor) {
-    *mayor = *number;
+void comp(int* Number, int* Mayor){
+  if (*Number > *Mayor) {
+    *Mayor = *Number;
   }
 }
