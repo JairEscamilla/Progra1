2dabd6568e136c458aa39ab6686280bea47f65d4
Jair Escamilla <hernandez_yo14@outlook.es>
2018-09-13 17:07:07 -0500

    Instrucciones de practicas

---
+++ funciones/serie6.c
@@ -0,0 +1,18 @@
+#include <stdio.h> // Incluyo la libreria de entrada y salida
+// Prototipos de funciones
+void getVal();
+// Funcion principal
+int main(void){
+
+}
+
+/* Fundamentos -> Lab -> Practica1 (Aquí van a estar todos los ejercicios )
+  1.c
+  1.exe
+  2.c
+  2.exe
+  3.c
+  3.exe
+  Y así de manera sucesiva
+
+*/
