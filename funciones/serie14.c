4f3b21d2ee7452afb66d2b69ec191057b5c6c449
Jair Escamilla <hernandez_yo14@outlook.es>
2018-09-18 13:01:30 -0500

    Falta solucionar bugs en el ejercicio de la serie 14

--- funciones/serie14.c
+++ funciones/serie14.c
@@ -4,15 +4,21 @@ void getDate(long*);
 void getDay(long, int*);
 void getMonth(long, int*);
 void getYear(long, int*);
+void esBisiesto(int, int*);
+void tomorrow(int*, int*, int*, int*);
 // Funcion principal
 int main(void){
   long Fecha;
   int Dia, Mes, Anio;
+  int Bisiesto = 0;
+  long Manana;
   getDate(&Fecha);
   getDay(Fecha, &Dia);
   getMonth(Fecha, &Mes);
   getYear(Fecha, &Anio);
-  printf("%d/%d/%d\n", Dia, Mes, Anio);
+  esBisiesto(Anio, &Bisiesto);
+  tomorrow(&Dia, &Mes, &Anio, &Bisiesto);
+  printf("La fecha de mañana es %d/%d/%d\n", Dia, Mes, Anio);
 }
 
 // Desarrollando las funciones
@@ -31,4 +37,40 @@ void getMonth(long Fecha, int* Mes) {
 void getYear(long Fecha, int* Anio){
   int R = Fecha%10000;
   *Anio = R%100;
+}
+
+void esBisiesto(int Anio, int* Bisiesto){
+  if (Anio % 4 == 0 && Anio % 100 != 0 || Anio % 400 == 0 ) {
+    *Bisiesto = 1;
+  }
+}
+
+void tomorrow(int* Dia, int* Mes, int* Anio, int* Bisiesto){
+  if (*Dia == 30 && *Mes % 2 == 0 && *Mes != 8 && *Mes != 12 && *Mes != 2) {
+    *Dia = 1;
+    *Mes = *Mes + 1;
+  }else{
+    if (*Dia == 31 && *Mes %2 != 0 || *Mes == 8) {
+      *Dia = 1;
+      *Mes = *Mes + 1;
+    }else{
+      if (*Dia == 31 && *Mes == 12) {
+        *Dia = 1;
+        *Mes = 1;
+        *Anio = *Anio + 1;
+      }else{
+        if (*Dia == 29 && *Mes == 2 && Bisiesto) {
+          *Dia = 1;
+          *Mes = *Mes + 1;
+        }else{
+          if (*Dia == 28 && *Mes == 2 && Bisiesto == 0) {
+            *Dia = 1;
+            *Mes = *Mes + 1;
+          }else{
+            *Dia = *Dia + 1;
+          }
+        }
+      }
+    }
+  }
 }
