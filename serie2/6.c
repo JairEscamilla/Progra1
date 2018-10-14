2f30bb8f1f28979529aa7c30d9eb2f038d0e7f99
Jair Escamilla <hernandez_yo14@outlook.es>
2018-10-13 22:12:56 -0500

    Ya funciona el 6

--- serie2/6.c
+++ serie2/6.c
@@ -2,10 +2,26 @@
 #include <stdio.h>
 #include <string.h>
 // Prototipos de funciones
-
+void invertir(char[]);
 // Funcion principal
 int main(void){
   char Frase[100];
   printf("Ingresar frase: ");
   gets(Frase);
+  invertir(Frase);
+}
+
+void invertir(char Frase[]){
+  for(int i = strlen(Frase)-1; i >= 0; i--){
+    if (Frase[i] == 32) {
+      for(int j = i+1; Frase[j] != '\0' && Frase[j] != 32; j++){
+        printf("%c", Frase[j]);
+      }
+      printf(" ");
+    }
+  }
+  for(int i = 0; Frase[i] != 32 && Frase[i] != '\0'; i ++){
+    printf("%c", Frase[i]);
+  }
+  printf("\n");
 }
