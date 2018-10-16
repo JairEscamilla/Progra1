34fc8d4d6969d47b82e729e86ba00d1e98a7fb20
Jair Escamilla <hernandez_yo14@outlook.es>
2018-10-16 17:31:34 -0500

    Buscando alumnos en ejemplo

--- ejemplos/multi.c
+++ ejemplos/multi.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 void leer(int[10][4]);
 void imprime(int[10][4]);
 void promedio(int[10][4]);
@@ -39,5 +40,20 @@ void imprime(int Cal[10][4]){
       printf("%d, ", Cal[j][i]);
     }
     printf("\n");
+  }
+}
+void BuscarAlumno(float Notas[], char Nombres[50][50], int cantidad){
+  int i = 0;
+  char Alumno[50];
+  printf("Que alumno buscas? ");
+  gets(Alumno);
+  while (i < cantidad && strcmp(Alumno, Nombres[i]) == 0) {
+    i++;
+  }
+  if (i == cantidad) {
+    printf("El alumno no se encuentra en la lista");
+  }else{
+    puts(Nombres[i]);
+    printf("%f\t %s\t\n", Notas[i], Nombres[i]);
   }
 }
