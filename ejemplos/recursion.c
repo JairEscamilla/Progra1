cb83efcf697d74d23e3da4f482678488dd419e3b
Jair Escamilla <hernandez_yo14@outlook.es>
2018-10-23 17:07:11 -0500

    Recursividad

---
+++ ejemplos/recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int factorial(int);
+int main(void){
+  int num;
+  int n;
+  printf("Ingresar numero: ");
+  scanf("%d", &num);
+  n = factorial(num);
+  printf("%d\n", n);
+}
+
+int factorial(int n){
+  if (n > 1) {
+    return n*factorial(n-1);
+  }else{
+    return 1;
+  }
+}
