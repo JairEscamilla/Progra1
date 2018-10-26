7938f54fa4a3b1b8e33e301df66da9a2e727cf42
Jair Escamilla <hernandez_yo14@outlook.es>
2018-10-25 22:38:00 -0500

    Hay que arreglar bugs

--- ejemplos/stringrecursivo.c
+++ ejemplos/stringrecursivo.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-void InvertirFrase(char Cadena[200]);
+void InvertirFrase(char Cadena[200], char Volteada[200]);
 int main(){
   char Frase[200];
+  char Volteada[200];
   printf("Ingresar frase: ");
   gets(Frase);
   printf("Tu frase invertida es : ");
-  InvertirFrase(Frase);
-  printf("\n");
+  InvertirFrase(Frase, Volteada);
+  puts(Volteada);
 }
 
-void InvertirFrase(char Cadena[200]) {
+void InvertirFrase(char Cadena[200], char Volteada[200]) {
   if (*Cadena != '\0') {
-    InvertirFrase(Cadena+1);
-    printf("%c", *Cadena);
+    InvertirFrase(Cadena+1, Volteada);
+
+    *Volteada = *Cadena;
+    Volteada ++;
   }
 }
