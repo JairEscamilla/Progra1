fb556b3f9af74ff83071a9fe1a8a0e25552cb94f
Jair Escamilla <hernandez_yo14@outlook.es>
2018-10-27 00:26:15 -0500

    Estructura base

---
+++ practica3/laberinto.c
@@ -0,0 +1,19 @@
+//  Incluimos las librerias
+#include <stdio.h>
+#include <string.h>
+
+// PROTOTIPOS DE FUNCIONES
+
+
+// FIN DE PROTOTIPOS DE FUNCIONES
+
+// FUNCION PRINCIPAL
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
+// FIN DE FUNCION PRINCIPAL
+
+// DESARROLLO DE LAS FUNCIONES
+
+// FIN DE DESARROLLO DE FUNCIONES
