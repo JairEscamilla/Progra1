36554296e9fe643024dc49fb6accb7b3ee68e955
Jair Escamilla <hernandez_yo14@outlook.es>
2018-11-03 00:47:09 -0600

    Imprimiendo array de laberinto

--- practica3/laberinto.c
+++ practica3/laberinto.c
@@ -3,22 +3,29 @@
 #include <string.h>
 
 // PROTOTIPOS DE FUNCIONES
-void leerArchivo(char Archivo[]);
+void leerArchivo(char Archivo[], char Laberinto[30][30], int* y);
+void imprimirLaberinto(char Laberinto[30][30], int y);
 // FIN DE PROTOTIPOS DE FUNCIONES
 
 // FUNCION PRINCIPAL
 int main(int argc, char *argv[]) {
   // Declaracion de variables
   char Archivo[50], Validacion[] = "-p";
+  char Laberinto[30][30];
+  int y;
   // Fin de declarcion de variables
-  if((argc < 2 || argc > 3) || (strcmp(argv[1], Validacion) == 0)){
+  if((argc < 2 || argc > 3) || (strcmp(argv[1], Validacion) == 0)){ // Validamos la cantidad de parametros y formato
     printf("Error, opcion incorrecta\n");
   }else{
-    if(argc == 3 && strcmp(argv[2], Validacion) != 0){
+    if(argc == 3 && strcmp(argv[2], Validacion) != 0){ // Validamos que el tercer parametro no sea distinto de -p
       printf("Error, opcion incorrecta\n");
     }else{
       strcpy(Archivo, argv[1]);
-      leerArchivo(Archivo);
+      leerArchivo(Archivo, Laberinto, &y); // Leemos el archivo
+      imprimirLaberinto(Laberinto, y);
+      printf("Presione enter para continuar... ");
+      getchar();
+      printf("Continuacion del problema\n");
     }
   }
 
@@ -28,13 +35,34 @@ int main(int argc, char *argv[]) {
 // FIN DE FUNCION PRINCIPAL
 
 // DESARROLLO DE LAS FUNCIONES
-void leerArchivo (char Archivo[]) { // Funcion que lee el archivo
+void leerArchivo (char Archivo[], char Laberinto[30][30], int* y) { // Funcion que lee el archivo
   FILE* Arch = fopen(Archivo, "r+b");
-  char c;
-  while (!feof(Arch)) {
+  char c; // Caracter que vamos a ir leyendo
+  int i = 0, j = 0;
+  // Copiamos el contenido del archivo a un arreglo bidimensional
+  while(!feof(Arch)){
     fread(&c, sizeof(char), 1, Arch);
-    printf("%c", c);
+    Laberinto[j][i] = c;
+    if (c == '\n') {
+      j++;
+      i = -1;
+    }
+    i++;
   }
+
+  *y = j;
   fclose(Arch);
+}
+
+void imprimirLaberinto(char Laberinto[30][30], int y){
+  int j = 0;
+  for(int i = 0; i < y-1; i++){
+    j = 0;
+    while (Laberinto[i][j] != '\n') {
+      printf("%c", Laberinto[i][j]);
+      j++;
+    }
+    printf("\n");
+  }
 }
 // FIN DE DESARROLLO DE FUNCIONES
