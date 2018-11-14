72a8bbaac6b0c76be14f64de968a8e42ac71d8ce
Jair Escamilla <hernandez_yo14@outlook.es>
2018-11-14 14:21:07 -0600

    Validando la existencia del archivo de login

--- Proyecto/bicirenta.c
+++ Proyecto/bicirenta.c
@@ -4,28 +4,53 @@
 #include <string.h>
 // Definimos las estructuras
 //******************************************************************************
-
-
+typedef struct defLogin{
+  char Nombre[50];
+  char Direccion[200];
+  char Contrasenia[50];
+  long TarjetaCredito;
+  long UserNumber;
+  int Flag;
+}User;
 //******************************************************************************
 
 
-// Prototipos de las funciones 
+// Prototipos de las funciones
 //******************************************************************************
-
-
+void validar_archivo_login();
 //******************************************************************************
 
 
 // Funcion principal
 //******************************************************************************
 int main(int argc, char *argv[]) {
-  /* code */
+  int TipoUsuario = 0;
+  if (argc != 1) {
+    printf("Esto se va a desarrollar despues\n");
+  }else{
+    validar_archivo_login();
+  }
   return 0;
 }
 //******************************************************************************
 
 // Desarrollo de las funciones
 //******************************************************************************
-
-
+void validar_archivo_login(){
+  User Usuario;
+  FILE* Arch;
+  FILE* Archivo;
+  Arch = fopen("login.txt", "rt");
+  if (Arch == NULL) {
+    Archivo = fopen("login.txt", "wt");
+    strcpy(Usuario.Nombre, "Ibero");
+    strcpy(Usuario.Direccion, "Prolongacion Paseo de la Reforma 880, Lomas de Santa Fe, 01219 Ciudad de Mexico, CDMX");
+    strcpy(Usuario.Contrasenia, "c123");
+    Usuario.TarjetaCredito = 1234567;
+    Usuario.UserNumber = 1;
+    Usuario.Flag = 1;
+    fprintf(Archivo, "%s\n%s\n%s\n%ld\n%ld\n%d\n", Usuario.Nombre, Usuario.Direccion, Usuario.Contrasenia, Usuario.TarjetaCredito, Usuario.UserNumber, Usuario.Flag);
+    fclose(Archivo);
+  }
+}
 //******************************************************************************
