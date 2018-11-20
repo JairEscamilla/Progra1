ee2002a3fd1786d01bb2db90b1419fb1f6fc9ac1
Jair Escamilla <hernandez_yo14@outlook.es>
2018-11-20 16:27:47 -0600

    funciones de listas dinamicas

--- estructuras/funcioneslistas.c
+++ estructuras/funcioneslistas.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
+#include <stdio.h>
 typedef struct defElemento{
   int dato;
   struct defElemento* siguiente;
 }TipoElemento;
 void InsertaInicio(TipoElemento** Inicio, int numero);
+void imprime(TipoElemento* Inicio);
+void BorrarLista(TipoElemento* Inicio);
 
 void InsertaInicio(TipoElemento** Inicio, int numero){
   TipoElemento* temp;
@@ -11,4 +14,35 @@ void InsertaInicio(TipoElemento** Inicio, int numero){
   temp->dato = numero;
   temp->siguiente = *Inicio;
   *Inicio = temp;
+}
+
+void imprime(TipoElemento* Inicio){
+  TipoElemento* temp = Inicio;
+  while (temp != NULL) {
+    printf("%d, ", temp->dato);
+    temp = temp->siguiente;
+  }
+}
+
+void BorrarLista(TipoElemento* Inicio){
+  TipoElemento* temp;
+  temp = Inicio;
+  while (temp != NULL) {
+    Inicio = Inicio->siguiente;
+    free(temp);
+    temp = Inicio;
+  }
+}
+
+int main(){
+  TipoElemento* lista = NULL;
+  int valor;
+  printf("Inserte numeros, letra para terminar: ");
+  while (scanf("%d", &valor) == 1) {
+    InsertaInicio(&lista, valor);
+  }
+  printf("Los valores introducidos son: \n");
+  imprime(lista);
+  printf("\n");
+  BorrarLista(lista);
 }
